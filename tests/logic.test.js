cd3e4926840cdc353fccd4641d3b3719f5ce0bb7
Elen <elcrses@ya.ru>
2019-11-22 01:20:08 +0300

    add end game tests and merge tests

--- tests/logic.test.js
+++ tests/logic.test.js
@@ -79,9 +79,10 @@ describe("first move tests", () => {
 
 describe("some turns tests", () => {
     test("game is not over after some turns", () => {
+        const SOME_TURNS = 5;
         let testGame = game();
         let directions = ['left', 'up', 'right', 'down'];
-        for (let turnNumber = 0; turnNumber < 5; turnNumber++) {
+        for (let turnNumber = 0; turnNumber < SOME_TURNS; turnNumber++) {
             testGame.turn(directions[Math.floor(Math.random()*directions.length)]);
         }
         
@@ -89,10 +90,11 @@ describe("some turns tests", () => {
     });
 
     test("game max score is correct after some turns", () => {
+        const SOME_TURNS = 5;
         let testGame = game();
         let directions = ['left', 'up', 'right', 'down'];
         let board = [];
-        for (let turnNumber = 0; turnNumber < 5; turnNumber++) {
+        for (let turnNumber = 0; turnNumber < SOME_TURNS; turnNumber++) {
             board = testGame.turn(directions[Math.floor(Math.random()*directions.length)]);
         }
 
@@ -107,36 +109,145 @@ describe("some turns tests", () => {
 });
 
 describe("custom board tests", () => {
+    let notEmptyValuesCount = board => 
+        board.reduce((count, row) => 
+            count + row.reduce((rowCount, tile) => {
+            if (tile > 0)
+                return rowCount + 1;
+            return rowCount;     
+            }, 0), 
+        0); 
+
+    let executeTest = (beforeBoard, afterBoard, direction) => {
+        let testGame = game(beforeBoard);
+        const turnBoard = testGame.turn(direction);
+
+        const isMovedCorrect = turnBoard.every((row, i) => 
+            row.every((tile, j) => 
+                (afterBoard[i][j] !== 0 && tile === afterBoard[i][j]) ||
+                (afterBoard[i][j] === 0)
+            ));
+        const valuesCount = notEmptyValuesCount(turnBoard);
+
+        return {isMovedCorrect, valuesCount}
+    } 
+
     test("move to the left with merge", () => {
-        const testBoard = [
+        const BEFORE_BOARD = [
             [8, 4, 4, 4],
             [4, 4, 4, 4],
             [0, 0, 2, 0],
             [8, 0, 0, 8]
         ];
-        const board = [
+        const AFTER_BOARD = [
             [8, 8, 4, 0],
             [8, 8, 0, 0],
             [2, 0, 0, 0],
             [16, 0, 0, 0]
         ];
-        let testGame = game(testBoard);
-        const turnBoard = testGame.turn('left');
+        const {isMovedCorrect, valuesCount} = executeTest(BEFORE_BOARD, AFTER_BOARD, 'left');
 
-        const movedCorrect = turnBoard.every((row, i) => 
-            row.every((tile, j) => 
-                (board[i][j] !== 0 && tile === board[i][j]) ||
-                (board[i][j] === 0)
-            ));
-        expect(movedCorrect).toBe(true);
-
-        const valuesCount = turnBoard.reduce((count, row) => 
-            count + 
-            row.reduce((rowCount, tile) => {
-                if (tile > 0)
-                    return rowCount + 1;
-                return rowCount;     
-            }, 0), 0);
-        expect(valuesCount).toBe(8);
+        expect(isMovedCorrect).toBe(true);
+        expect(valuesCount).toBe(notEmptyValuesCount(AFTER_BOARD) + 1);
     });
+
+    test("move to the right with merge", () => {
+        const BEFORE_BOARD = [
+            [8, 4, 4, 4],
+            [4, 4, 4, 4],
+            [0, 0, 2, 0],
+            [8, 0, 0, 8]
+        ];
+        const AFTER_BOARD = [
+            [0, 8, 4, 8],
+            [0, 0, 8, 8],
+            [0, 0, 0, 2],
+            [0, 0, 0, 16]
+        ];
+        const {isMovedCorrect, valuesCount} = executeTest(BEFORE_BOARD, AFTER_BOARD, 'right');
+
+        expect(isMovedCorrect).toBe(true);
+        expect(valuesCount).toBe(notEmptyValuesCount(AFTER_BOARD) + 1);
+    });
+
+    test("move to the up with merge", () => {
+        const BEFORE_BOARD = [
+            [8, 4, 4, 4],
+            [4, 4, 4, 4],
+            [0, 0, 2, 0],
+            [8, 0, 0, 8]
+        ];
+        const AFTER_BOARD = [
+            [8, 8, 8, 8],
+            [4, 0, 2, 8],
+            [8, 0, 0, 0],
+            [0, 0, 0, 0]
+        ];
+        const {isMovedCorrect, valuesCount} = executeTest(BEFORE_BOARD, AFTER_BOARD, 'up');
+
+        expect(isMovedCorrect).toBe(true);
+        expect(valuesCount).toBe(notEmptyValuesCount(AFTER_BOARD) + 1);
+    });
+
+    test("move to the down with merge", () => {
+        const BEFORE_BOARD = [
+            [8, 4, 4, 4],
+            [4, 4, 4, 4],
+            [0, 0, 2, 0],
+            [8, 0, 0, 8]
+        ];
+        const AFTER_BOARD = [
+            [0, 0, 0, 0],
+            [8, 0, 0, 0],
+            [4, 0, 8, 8],
+            [8, 8, 2, 8]
+        ];
+        const {isMovedCorrect, valuesCount} = executeTest(BEFORE_BOARD, AFTER_BOARD, 'down');
+
+        expect(isMovedCorrect).toBe(true);
+        expect(valuesCount).toBe(notEmptyValuesCount(AFTER_BOARD) + 1);
+    });
+});
+
+describe('End game tests', () => {
+    test('Game is not over. Board has empty tiles', () => {
+        const BOARD = [
+            [0, 2, 8, 16],
+            [8, 32, 256, 2],
+            [4, 8, 32, 2],
+            [2, 4, 2, 4]
+        ];
+
+        expect(game(BOARD).isOver()).toBe(false);
+    });
+
+    test('Game is not over. Board has tiles to be merged', () => {
+        const BOARD = [
+            [2, 8, 2, 8],
+            [32, 2, 8, 32],
+            [2, 4, 2, 32],
+            [8, 2, 8, 2]
+        ];
+        expect(game(BOARD).isOver()).toBe(false);
+    });
+
+    test('Game is over. There are no more empty tiles and no merge', () => {
+        const BOARD = [
+            [32, 2, 32, 4],
+            [16, 4, 2, 256],
+            [8, 2, 4, 64],
+            [16, 8, 2, 4]
+        ];
+        expect(game(BOARD).isOver()).toBe(true);
+    });
+
+    test('Game is over. Max value tile is 2048', () => {
+        const BOARD = [
+            [2, 2, 4, 2],
+            [2048, 0, 0, 0],
+            [256, 64, 16, 0],
+            [256, 0, 16, 0]
+        ];
+        expect(game(BOARD).isOver()).toBe(true);
+    })
 });
