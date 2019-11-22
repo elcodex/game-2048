8c14fae4e364ce17ee42e66f712dbcc21a7a9949
Elen <elcrses@ya.ru>
2019-11-22 23:26:25 +0300

    game.js: fix sudden ending screen during the game

--- game.js
+++ game.js
@@ -1,46 +1,44 @@
-const playerTurn = game => {
-  const handleKeydownEvent = event => {
+const handleKeydownEvent = event => {
     //console.log(board);
     let direction = '';
     if (event.code === 'ArrowDown'  || event.code === 'KeyS') {
-      //console.log('down');
-      direction = 'down';
+        //console.log('down');
+        direction = 'down';
     }
     if (event.code === 'ArrowLeft'  || event.code === 'KeyA') {
-      //console.log('left');
-      direction = 'left';
+        //console.log('left');
+        direction = 'left';
     }
     if (event.code === 'ArrowUp'    || event.code === 'KeyW') {
-      //console.log('up');
-      direction = 'up';
+        //console.log('up');
+        direction = 'up';
     }
     if (event.code === 'ArrowRight' || event.code === 'KeyD') {
-      //console.log('right');
-      direction = 'right';
+        //console.log('right');
+        direction = 'right';
     }
     
     if (direction !== '') {
-      event.preventDefault();
+        event.preventDefault();
       
-      viewTurnBoard(game.turn(direction));
-      if (game.isOver()) {
-        setTimeout(_ => {
-          viewGameOverBoard(game.maxScore());
-          document.removeEventListener('keydown', handleKeydownEvent);
-        }, 500);
-      }
+        viewTurnBoard(game2048.turn(direction));
+        if (game2048.isOver()) {
+            setTimeout(() => {
+                viewGameOverBoard(game2048.maxScore());
+                document.removeEventListener('keydown', handleKeydownEvent);
+            }, 500);
+        }
         
     }
-  }
-
-  document.addEventListener('keydown', handleKeydownEvent);
 }
 
-const play = _ => {
-  let game2048 = game();
-  viewStartBoard(game2048.start());
-  playerTurn(game2048);
-}
+let game2048 = game();
 
-play();
-document.querySelector('.btn-newgame').addEventListener('click', e => play());
+viewStartBoard(game2048.start());
+document.addEventListener('keydown', handleKeydownEvent);
+
+document.querySelector('.btn-newgame').addEventListener('click', e => {
+    document.removeEventListener('keydown', handleKeydownEvent);
+    viewStartBoard(game2048.start());
+    document.addEventListener('keydown', handleKeydownEvent);
+});
