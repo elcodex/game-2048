bcf68e384516dfb03bd9324bd796f406b628c92f
Elen <elcrses@ya.ru>
2021-02-17 18:44:23 +0300

    refactoring random-play.test.js

--- tests/random-play.test.js
+++ tests/random-play.test.js
@@ -1,25 +1,25 @@
-const game = require('../scripts/logic.js');
+const Game = require('../scripts/logic.js');
 
 describe('play the game', () => {
-    const randomGameTest = (randomGame) => {
+    const randomGameTest = () => {
         const DIRECTIONS = ['left', 'right', 'up', 'down'];
-        //let randomGame = game();
-        let board = randomGame.start();
+        let randomGame = new Game();
+        randomGame.start();
         while (!randomGame.isOver()) {
             const direction = DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)];
-            board = randomGame.turn(direction);
+            randomGame.turn(direction);
         }
         
-        expect(board.every(row => row.every(tile => tile > 0))).toBe(true);
+        expect(randomGame.board.every(row => row.every(tile => tile > 0))).toBe(true);
         
         const maxScore = randomGame.maxScore();
-        expect(board.every(row => row.every(tile => tile <= maxScore))).toBe(true);
+        expect(randomGame.board.every(row => row.every(tile => tile <= maxScore))).toBe(true);
+        expect(randomGame.board.some(row => row.some(tile => tile === maxScore))).toBe(true);
     }
 
-    test('play the random games some times', () => {
-        let randomGame = game();
-        for (let i = 0; i < 5; i++) {
-            randomGameTest(randomGame);
-        }
-    });
+    for (let i = 1; i <= 5; i++) {
+        test('play the random games some times' + ` (${i})`, () => {
+                randomGameTest();
+        });
+    }
 });
